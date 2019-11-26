45fc88171708b6d5af9dbbfccf315e1b5e4ebb3a
nxp72763 <anil.hiranniah@nxp.com>
2019-11-26 13:32:59 +0530

    Decrement channel count even if channel close fails
    SE Hal Deinit isn't triggered if channel count is not decremented
    leading to failure of FMCT-ARBT-99 test.
    
    Bug:142574946
    
    Test: VTS, FMCT
    Change-Id: I901dfeb9ddf37e2d50707770daaeb98fb9aa2354

--- 1.0/SecureElement.cpp
+++ 1.0/SecureElement.cpp
@@ -377,16 +377,13 @@ SecureElement::closeChannel(uint8_t channelNumber) {
     phNxpEse_free(rspApdu.p_data);
   }
 
-  if ((channelNumber == DEFAULT_BASIC_CHANNEL) ||
-      (sestatus == SecureElementStatus::SUCCESS)) {
-    if (mOpenedChannels[channelNumber] != false) mOpenedchannelCount--;
-    mOpenedChannels[channelNumber] = false;
-    /*If there are no channels remaining close secureElement*/
-    if (mOpenedchannelCount == 0) {
-      sestatus = seHalDeInit();
-    } else {
-      sestatus = SecureElementStatus::SUCCESS;
-    }
+  if (mOpenedChannels[channelNumber] != false) mOpenedchannelCount--;
+  mOpenedChannels[channelNumber] = false;
+  /*If there are no channels remaining close secureElement*/
+  if (mOpenedchannelCount == 0) {
+    sestatus = seHalDeInit();
+  } else {
+    sestatus = SecureElementStatus::SUCCESS;
   }
   return sestatus;
 }
